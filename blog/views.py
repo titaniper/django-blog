e75da7983ffe3fa966069c843d9b08ef3839a7c5
titaniper <jy.kang@ecubelabs.com>
2022-04-08 15:24:17 +0900

    up

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import  Post
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+
+class PostDetail(DetailView):
+    model = Post
+    # template_name = 'blog/views_single_page.html'
 
 def index(request):
     posts = Post.objects.all().order_by('-pk')
