35810e169e536d6ba319c585a5b37f4b4443f4a1
titaniper <jy.kang@ecubelabs.com>
2022-04-15 15:13:27 +0900

    feat: Image header, category

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,13 @@ import os
 # from django.db import models
 
 # Create your models here.
+
+Class Tag
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    # NOTE: 한글 입력 allow_unicode ture
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
     def __str__(self):
@@ -16,13 +21,14 @@ class Category(models.Model):
         return f'/blog/category/{self.pk}'
 
     class Meta:
-        verbose_name_plural : 'Categories'
+        verbose_name_plural: 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', null=True, blank=True)
+    attached_file = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
